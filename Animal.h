30a548e6065aa0140dd11b3f934329e1734ec0ae
khadijahamdy <khadijahamdy29@gmail.com>
2025-11-12 15:14:19 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -4,8 +4,23 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
-
+ 
+#include <iostram>
+using namespace std;
+#ifndef ANIMAL_H
+#define ANIMAL_H
+class zoo{
+    private: 
+    string name;
+    int age;
+    bool isHungry;
+    public:
+    zoo();
+    zoo(string name,int age, bool isHungry);
+    void display();
+    void feed();
+}
+#endif
 
 
 #endif
