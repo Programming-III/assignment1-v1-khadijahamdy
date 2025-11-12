a4f42896416ea294808cc8ef4c47022b4510c502
khadijahamdy <khadijahamdy29@gmail.com>
2025-11-12 15:13:03 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -3,7 +3,19 @@
 
 #include "Animal.h"
 
-//define mammal class here
+#include <iostram>
+using namespace std;
+#ifndef MAMMAL_H
+#define MAMMAL_H
+#include "zoo.h"
+class mammal{
+    private:
+    string furColor;
+    public:
+    mammal();
+    mammal(string furColor);
+}
+#endif
 
 
 
