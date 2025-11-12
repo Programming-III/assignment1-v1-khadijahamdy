92eb4959331b31e34e1010194464730657515b6c
khadijahamdy <khadijahamdy29@gmail.com>
2025-11-12 15:13:39 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -3,7 +3,19 @@
 
 #include "Animal.h"
 
-//define class bird here 
+#include <iostram>
+using namespace std;
+#ifndef BIRD_H
+#define BIRD_H
+#include "zoo.h"
+class bird{
+    private:
+    float wingSpan;
+    public:
+    bird();
+    bird(float wingSpan);
+}
+#endif
 
 
 
