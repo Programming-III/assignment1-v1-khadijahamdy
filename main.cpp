cc4afdd710be1a4eb3bbec3a3568dbb5f5ef1884
khadijahamdy <khadijahamdy29@gmail.com>
2025-11-12 15:57:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,72 @@
 using namespace std;
 
 
-// ============== MAIN FUNCTION ==============
+using namespace std;
+#include <iostream>
+#include 'zoo.h'
+#include 'mammal.h'
+
+string animal::zoo(){
+    
+}
+string animal::zoo(string name,int age, bool isHungry){
+    this-> name=name;
+    this-> age = age;
+    this-> isHungry = isHungry;
+}
+void animal::display(){
+    cout<<name<<endl;
+    cout<<age<<endl;
+    cout<<isHungry<<endl;
+    
+}
+void animal::feed(){
+    if(animal==isHungry);
+    cout<<isHungry<<endl;
+    
+}
+void animal::setname(int name){
+ int name=name;}
+ void animal::setage(int age){
+     int age=age;}
+     void animal::setisHungry(bool s){
+         bool s=setisHungry;
+     }
+   string animal::getname(){
+     return name;
+ }
+    int animal::getage(){
+     return age;
+ }
+ bool animal:: gethungry(){
+     return isHungry();
+ }
+ string  mammal:: mammal(){}
+  string  mammal:: mammal(string furColor){
+      this->furColor=furColor;
+  }
+  void mammal:: setfurColor(int s){
+      int s=setfurColor;
+  }
+ string mammal::getfurColor(){
+     return furColor;
+ }
+ string bird::bird(){}
+ string bird::bird(float wingSpan){
+     this->wingSpan=wingSpan;
+ }
+ void bird:: setwing(float b){
+     float b=setwing();
+ }
+ string bird::getwing(){
+     return setwing;
+ }
+int main()
+{
+    std::cout<<"Hello World";
+
+    return 0;
+}
 int main() {
     
     
